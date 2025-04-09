dbaf4effafd51cfd7923e56964441d5220af82e6
miloscole <27miloscole@gmail.com>
2025-04-09 14:54:11 +0200

    Add UnitTests project, handle unit tests for Login presenter

---
+++ MachineApp.UnitTests/Presenters/LoginPresenterTests.cs
@@ -0,0 +1,65 @@
+﻿using MachineApp.Models;
+using MachineApp.Presenters;
+using MachineApp.Repositories;
+using MachineApp.Views.Login;
+using Moq;
+
+namespace MachineApp.UnitTests.Presenters
+{
+    [TestClass]
+    public class LoginPresenterTests
+    {
+        private Mock<ILoginView> mockView = null!;
+        private Mock<IUserRepo> mockRepo = null!;
+        private LoginPresenter presenter = null!;
+        private readonly String userCredential = "credential";
+
+        [TestInitialize]
+        public void Setup()
+        {
+            mockView = new Mock<ILoginView>();
+            mockRepo = new Mock<IUserRepo>();
+            presenter = new LoginPresenter(mockView.Object, mockRepo.Object);
+        }
+
+        [TestMethod]
+        public void ShowsError_When_InvalidCredentials()
+        {
+            mockView.Setup(v => v.Username).Returns(userCredential);
+            mockView.Setup(v => v.Password).Returns(userCredential);
+            mockRepo.Setup(r => r.GetUser(userCredential, userCredential)).Returns((User?)null);
+
+            presenter.Login();
+
+            mockView.Verify(v => v.ShowError("Invalid username or password."), Times.Once);
+        }
+
+        [TestMethod]
+        public void ShowsWelcome_When_ValidCredentials()
+        {
+            var role = "Admin";
+
+            mockView.Setup(v => v.Username).Returns(userCredential);
+            mockView.Setup(v => v.Password).Returns(userCredential);
+
+            var user = new User { Username = userCredential, Role = role };
+            mockRepo.Setup(r => r.GetUser(userCredential, userCredential)).Returns(user);
+
+            presenter.Login();
+
+            mockView.Verify(v => v.ShowWelcome(userCredential, role), Times.Once);
+        }
+
+        [TestMethod]
+        public void ShowsUnexpectedError_When_RepositoryThrowsException()
+        {
+            mockView.Setup(v => v.Username).Returns(userCredential);
+            mockView.Setup(v => v.Password).Returns(userCredential);
+            mockRepo.Setup(r => r.GetUser(userCredential, userCredential)).Throws(new Exception("DB failure"));
+
+            presenter.Login();
+
+            mockView.Verify(v => v.ShowError(It.Is<string>(msg => msg.Contains("Unexpected error"))), Times.Once);
+        }
+    }
+}
---
+++ MachineApp/Presenters/LoginPresenter.cs
@@ -0,0 +1,33 @@
+﻿
+using MachineApp.Views.Login;
+using MachineApp.Repositories;
+
+namespace MachineApp.Presenters
+{
+    public class LoginPresenter(ILoginView view, IUserRepo repo)
+    {
+        private readonly ILoginView _view = view;
+        private readonly IUserRepo _repo = repo;
+
+        public void Login()
+        {
+            try
+            {
+                var user = _repo.GetUser(_view.Username, _view.Password);
+                if (user == null)
+                {
+                    _view.ShowError("Invalid username or password.");
+                }
+                else
+                {
+                    _view.ShowWelcome(user.Username, user.Role);
+                }
+            }
+            catch (Exception ex)
+            {
+                _view.ShowError($"Unexpected error: {ex.Message}");
+            }
+        }
+
+    }
+}
